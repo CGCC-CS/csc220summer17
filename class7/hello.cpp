03a2c14eab9af7ae25cb658d160ff806c954db3c
Wade Huber <wadehuber@gmail.com>
2017-06-21 20:08:17 -0700

    Class 7 updates

---
+++ class7/hello.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+
+// g++ --std=c++11 -Wall -pedantic hello.cpp
+
+using namespace std;
+
+int main() {
+  string msg = "Hello, world!";
+  cout << msg << endl;
+  return 0;
+}
