d9b95ce34f3b80984e0fdf08278ccd301664c1a5
Wade Huber <wadehuber@gmail.com>
2017-06-24 22:49:06 -0700

    Split staffmember

---
+++ class9/staff/staff.cpp
@@ -0,0 +1,48 @@
+// Adapted from an example from Java Foundations 
+//  by John Lewis, Peter DePasquale, & Joe Chase
+//
+#include<iostream>
+#include"staffmember.hpp"
+#include"salaried.hpp"
+#include"volunteer.hpp"
+#include"hourly.hpp"
+
+using namespace std;
+
+#define STAFF_SIZE  4
+
+void print_paystub(StaffMember * s) {
+  s->print();
+  cout << "  Current pay : " << s->pay() << endl;
+}
+
+int main() {
+  int ii;
+  int totalpay = 0;
+  Salaried *  emp1 = new Salaried("Bob Smith", "123 Main", "480-555-8765", 
+                           "111-22-3333", 50000);
+  Volunteer * emp2 = 
+          new Volunteer("Alice Jenkins", "456 4th", "480-555-2341");
+  Hourly * emp3 = 
+          new Hourly("Mitch Jenkins", "456 4th", "480-555-2341", 
+                          "222-33-2222", 10, 40);
+
+  StaffMember * staff[STAFF_SIZE];
+  staff[0] = emp1;
+  staff[1] = emp2;
+  staff[2] = emp3;
+  staff[3] = new Salaried("John Franklin", "222 Bank", "480-555-2343", 
+                           "111-22-4444", 78000);
+
+  for(ii=0;ii<STAFF_SIZE;ii++)  {
+    cout <<"\nEmployee " << ii << ": " << endl;
+    print_paystub(staff[ii]);
+    totalpay=totalpay + staff[ii]->pay();
+  }
+
+  cout << "\nTotal payroll: " << totalpay << endl; 
+  cout << "Done!" << endl;  
+
+  return 0;
+
+}
